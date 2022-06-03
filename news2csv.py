50666971a1532bfdf5679e8ae603d9b62a494193
alejandraprj <aperearojas@college.harvard.edu>
2022-06-03 09:50:47 -0400

    news2csv

--- news2csv.py
+++ news2csv.py
@@ -1,8 +1,9 @@
-import pandas as pd
+#!/usr/local/bin/python3
 import csv
+import pandas as pd
 from pygooglenews import GoogleNews
 
-gn = GoogleNews (lang = 'en', country = 'UK') 
+gn = GoogleNews() #lang = 'en', country = 'US'
 
 Xmassearch = gn.search('intitle:Christmas', helper = True, from_ = '2019-12-01', to_= '2019-12-31')
 
@@ -17,3 +18,7 @@ writer = csv.writer(file)
 writer.writerow(["Xmassearch"])
 
 file.close()
+# - finish code
+# - export first few, test
+# - revise 
+# - finish exporting everything

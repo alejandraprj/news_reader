98a88f975045f3dca992af24075b48cc09790d42
alejandraprj <aperearojas@college.harvard.edu>
2022-06-09 14:08:12 -0400

    sub final

--- news2csv.py
+++ news2csv.py
@@ -18,7 +18,7 @@ def cleanhtml(raw_html):
 d = []
 
 # Access the feed and store data in d
-def readFeed(url):
+def readFeed(url, category):
 
     feed = feedparser.parse(url)
 
@@ -33,7 +33,7 @@ def readFeed(url):
 
         description = cleanhtml(post.summary)
         source = post.source.title
-        d.append((title, link, pubDate, description, source))
+        d.append((title, link, pubDate, description, source, category))
         # print(d)
     
     # Add delay between calls
@@ -41,20 +41,21 @@ def readFeed(url):
     return d
 
 # Converts to CSV
-def tocsv(category, filename, url):
+def tocsv(category, url):
 
     print("Reading now: ", category)
-    readFeed(url)
+    readFeed(url, category)
 
-    df = pd.DataFrame(d, columns=('Title', 'Link', 'pubDate', 'Description','Source'))
+    df = pd.DataFrame(d, columns=('Title', 'Link', 'pubDate', 'Description','Source', 'Category'))
 
     # Remove all rows with the same link - you might want to comment this when using different keywords
     df.drop_duplicates(subset ="Link", keep = False, inplace = True)
             
     # Store data to CSV
+    filename = category + ".csv"
     df.to_csv(filename, encoding='utf-8', index=False)
     print(len(df), "Articles saved on ", filename)
 
 urls = pd.read_csv('feeds.csv', sep=',')
 for i, row in urls.iterrows():
-    tocsv(row[0], row[1], row[2])
+    tocsv(row[0], row[1])
